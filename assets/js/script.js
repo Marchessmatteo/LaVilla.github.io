9f04892953e2011d386db400296351b28305f8f2
Marchessmatteo <matteo98.marchesani@gmail.com>
2023-02-24 23:20:06 +0100

    scrol navbar
    servizi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
+$(document).on('scroll', function() {
+    if($(this).scrollTop() > 50) {
+      $('.navbar').addClass('navbar-scrolled');
+    } else {
+      $('.navbar').removeClass('navbar-scrolled');
+    }
+  });
 
  
