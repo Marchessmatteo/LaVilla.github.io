c483613fdb369229e89b5020b8c0cee8e0eea756
Marchessmatteo <matteo98.marchesani@gmail.com>
2023-03-08 21:49:41 +0100

    storia
    book

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,34 @@ $(document).on('scroll', function() {
       $('.navbar').removeClass('navbar-scrolled');
     }
   });
-  
 
- 
+
+  var pages = document.getElementsByClassName('page');
+  for(var i = 0; i < pages.length; i++) {
+    var page = pages[i];
+    if (i % 2 === 0) {
+      page.style.zIndex = (pages.length - i);
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function(){
+    for(var i = 0; i < pages.length; i++) {
+      pages[i].pageNum = i + 1;
+      pages[i].onclick=function() {
+        if (this.pageNum % 2 === 0) {
+          this.classList.remove('flipped');
+          this.previousElementSibling.classList.remove('flipped');
+        } else {
+          this.classList.add('flipped');
+          this.nextElementSibling.classList.add('flipped');
+        }
+  
+        if (this.pageNum === pages.length) {
+          setTimeout(function() {
+            pages[0].classList.remove('flipped');
+          }, 1000);
+        }
+      }
+    }
+  });
+  
